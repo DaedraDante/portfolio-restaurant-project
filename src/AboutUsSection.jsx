7094fb88893bbed568655973fd43fbf3637aecf7
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-04-27 21:28:22 +0300

    Updated 5 , improved about us section

--- src/AboutUsSection.jsx
+++ src/AboutUsSection.jsx
@@ -3,9 +3,11 @@ function AboutUsSection() {
   return (
 <section className="about-us-section container">
   <h2>About Us</h2>
-    <div className="divider" />
-    <span className="proverb">一期一会</span>
-    <p className="proverb-translation">“Once in a lifetime encounter”</p>
+    <div className="divider"></div>
+    <div className="proverb-wrapper">
+       <span className="proverb"> 期 会</span>
+       <p className="proverb-translation">“Once in a lifetime encounter”</p>
+    </div>
     <p>
       Nestled in the heart of the city, our restaurant is a haven for lovers of authentic Japanese cuisine. 
       We honor time-honored techniques and seasonal ingredients, offering a dining experience that’s both elegant and soulful.
