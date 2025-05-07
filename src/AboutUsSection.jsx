21e7ce1b0377ab333d4e00933bb389c94e8926be
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-05-07 10:18:56 +0300

    Update 9 about us layout
    fiddled with the responsiveness of the layout on the about us section , good to be back

--- src/AboutUsSection.jsx
+++ src/AboutUsSection.jsx
@@ -10,7 +10,7 @@ function AboutUsSection() {
        <span className="proverb"> 期 会</span>
        <p className="proverb-translation">“Once in a lifetime encounter”</p>
     </div>
-    <p>
+    <p className="about-us-text">
       Nestled in the heart of the city, our restaurant is a haven for lovers of authentic Japanese cuisine. 
       We honor time-honored techniques and seasonal ingredients, offering a dining experience that’s both elegant and soulful.
       Every detail, from the ambiance to the dishes, reflects our deep respect for Japanese culture and culinary tradition.
